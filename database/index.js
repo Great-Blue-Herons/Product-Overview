fead709ecee43d7ed85f28106f0c9692d7be7b31
Joy <parkerjn90@gmail.com>
2022-06-07 20:05:58 -0400

    add dependencies and begin database index file for creating tables and adding data to them

---
+++ database/index.js
@@ -0,0 +1,28 @@
+// create new products table if doesn't exist yet
+CREATE TABLE products (
+  id int,
+  name text,
+  slogan text,
+  description text,
+  category text,
+  default_price text,
+  created_at timestamp,
+  updated_at timestamp
+);
+// insert values into products rows
+
+// create new features table if doesn't exist yet
+
+// insert values into features rows
+
+// create new styles table if doesn't exist yet
+
+// insert values into styles rows
+
+// create new skus table if doesn't exist yet
+
+// insert values into skus rows
+
+// create new photos table if doesn't exist yet
+
+// insert values into photos rows
