579f6426add4632d0ba2ee6b7d78bac0c4d34dc7
Joy <parkerjn90@gmail.com>
2022-06-09 11:55:15 -0400

    begin new client to start connection between server and db

--- database/index.js
+++ database/index.js
@@ -1,28 +1,9 @@
-// create new products table if doesn't exist yet
-CREATE TABLE products (
-  id int,
-  name text,
-  slogan text,
-  description text,
-  category text,
-  default_price text,
-  created_at timestamp,
-  updated_at timestamp
-);
-// insert values into products rows
-
-// create new features table if doesn't exist yet
-
-// insert values into features rows
-
-// create new styles table if doesn't exist yet
-
-// insert values into styles rows
-
-// create new skus table if doesn't exist yet
-
-// insert values into skus rows
-
-// create new photos table if doesn't exist yet
-
-// insert values into photos rows
+require('dotenv').config();
+const { Client } = require('pg');
+const client = new Client({
+  user: process.env.USER,
+  password: process.env.PASSWORD, // put in .env file
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE
+});
