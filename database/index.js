b51fb0c6dcff847054766f472af547c0dbb790b1
Joy <parkerjn90@gmail.com>
2022-06-10 15:25:56 -0400

    queries for all, one written, query for related written (sends back an array within an object within an array, and query for features sends back an array of objects with info from both tables combined in the same object

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { Client } = require('pg');
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
@@ -8,7 +8,7 @@ const client = new Client({
   database: process.env.DATABASE
 });
 
-// client.connect()
+// pool.connect()
 //   .then(() => {
 //     console.log('Connected successfully');
 //   })
@@ -18,28 +18,88 @@ const client = new Client({
 
 
 // query for all products (based on page and count)
+
 const queryAll = (row_count = 5, page = 0) => {
-  if (page > 0 ) {
+  if (page > 0) {
     page = page - 1;
   };
-  client.connect()
-  client.query(`select * from all_products limit ${row_count} offset ${page * row_count}`)
-    .then((results) => { console.log(results.rows) })
-    .catch((err) => { console.error(err) })
-    .finally(() => client.end());
+  pool
+    .query(`select * from all_products limit ${row_count} offset ${page * row_count};`)
+    .then((results) => {
 
+      console.log(results.rows);
+    })
+    .catch((err) => {
+      console.error(err)
+
+    })
 };
 
-// queryAll();
+queryAll();
 
 // query for specific product and details
 
 const queryOne = (product_id) => {
-  client.connect()
-  client.query(`select * from all_products where id = ${product_id}`)
-    .then((result) => {console.log(result.rows)})
-    .catch((err) => {console.error(err)})
-    .finally(() => client.end());
+  pool
+    .query(`select * from all_products where id = ${product_id};`)
+    .then((result) => {
+      console.log(result.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 queryOne(1);
+
+// write query to just return all details and features for one product
+
+const queryFeatures = (product_id) => {
+  pool
+    .query(`select *
+        from all_products
+        inner join features
+        on all_products.id = features.product_id
+        where all_products.id = ${product_id};`)
+    .then((results) => {
+      console.log(results.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+queryFeatures(1);
+
+// const queryStyles = (product_id) => {
+//   pool.connect()
+//     .then((client) => {
+//       return client
+//         .query(``)
+//         .then((results) => {
+//           client.release();
+//           console.log(results.rows);
+//         })
+//         .catch((err) => {
+//           console.error(err);
+//           client.release();
+//         });
+//     });
+// };
+
+// queryStyles(1);
+
+const queryRelated = (product_id) => {
+  pool
+    .query(`select json_agg(related_product_id)
+        from related where current_product_id
+        = ${product_id};`)
+    .then((results) => {
+      console.log(results.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+queryRelated(1);
