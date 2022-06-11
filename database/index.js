38642d006181ba2478ba413b66491cfdec99af07
Joy <parkerjn90@gmail.com>
2022-06-11 11:05:06 -0400

    working styles query, nests related photos and skus appropriately

--- database/index.js
+++ database/index.js
@@ -47,7 +47,7 @@ const queryOne = (product_id) => {
     .catch((err) => {
       console.error(err);
     });
-    // console.log(allInfo);
+  // console.log(allInfo);
 };
 
 // queryOne(1);
@@ -67,23 +67,83 @@ const queryFeatures = (product_id) => {
 
 // queryFeatures(2);
 
-const queryStyles = (product_id) => {
+// query one product with features
+
+const queryWithFeatures = (product_id) => {
   pool.connect()
-    .then((client) => {
-      return client
-        .query(``)
+    .query(`select json_build_object(
+      'id', a.id,
+      'name', a.name,
+      'slogan', a.slogan,
+      'description', a.description,
+      'category', a.category,
+      'default_price', a.default_price,
+      'features', (
+        select json_agg(json_build_object(
+          'feature', f.feature,
+          'value', f.value
+        ))
+        from features f where f.product_id = a.id
+      )
+    ) as result
+    from all_products a
+    where a.id = ${product_id};`)
+    .then((results) => {
+      console.log(results.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+// queryWithFeatures(1);
+
+// query all styles for one product
+
+const queryStyles = (product_id) => {
+    return pool
+        .query(`select json_build_object(
+          'product_id', al.id,
+          'results', (
+              select json_agg(json_build_object(
+          'style_id', st.id,
+          'name', st.name,
+          'original_price', st.original_price,
+          'sale_price', st.sale_price,
+          'default?', st.default_style,
+          'photos', (
+              select json_agg(json_build_object(
+                  'thumbnail_url', ph.thumbnail_url,
+                  'url', ph.url
+              ))
+              from photos ph where ph.styleid = st.id),
+          'skus', (
+              select json_object_agg(
+                  sk.id, (select json_build_object(
+                      'quantity', sk.quantity,
+                      'size', sk.size)
+              from skus where "id" = sk.id)
+              )
+           from skus as sk where "styleid" = st.id
+           )
+      ))
+      as result
+      from styles st
+      where st.productid = al.id
+
+          )
+      ) as result
+      from all_products al
+      where al.id = ${product_id};`)
         .then((results) => {
-          client.release();
           console.log(results.rows);
         })
         .catch((err) => {
           console.error(err);
-          client.release();
         });
-    });
 };
 
-// queryStyles(1);
+queryStyles(1);
 
 const queryRelated = (product_id) => {
   return pool
@@ -91,7 +151,7 @@ const queryRelated = (product_id) => {
         from related where current_product_id
         = ${product_id};`)
     .then((results) => {
-      console.log(results.rows[0].json_agg);
+      console.log(results.rows);
     })
     .catch((err) => {
       console.error(err);
@@ -100,9 +160,13 @@ const queryRelated = (product_id) => {
 
 // queryRelated(1);
 
+
+
+// features async
+
 const findFeatures = (product_id) => {
   var obj = {};
-  ;(async () => {
+  ; (async () => {
     obj = await queryOne(product_id);
     obj.features = await queryFeatures(product_id);
     await console.log(obj);
