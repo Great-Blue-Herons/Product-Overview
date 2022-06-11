44a860c7b31eddf4b2b9a57c8fca07bd54a6326f
Joy <parkerjn90@gmail.com>
2022-06-11 11:47:51 -0400

    import query functions from database index file

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
 require('dotenv').config();
+const {
+  queryWithFeatures,
+  queryStyles,
+  queryRelated,
+  asyncFindFeatures
+} = require('../database/index.js');
 
 // write one function per query that will need to happen
