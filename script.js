ecea32e7caf7f616851cc245957f1ae8904788c1
Joy <parkerjn90@gmail.com>
2022-06-13 14:45:21 -0400

    update script file to batch test get requests

--- script.js
+++ script.js
@@ -2,16 +2,33 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 
-// export const options = {
-//   stages: [
-//     { duration: '30s', target: 20 },
-//     { duration: '1m30s', target: 10 },
-//     { duration: '20s', target: 0 },
-//   ],
-// };
+export const options = {
+  stages: [
+    { duration: '30s', target: 20 },
+    // { duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
+};
 
 export default function () {
-  const res = http.get('http://localhost:3000/products/1');
-  check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
+  let id = 9;
+  const product = {
+    method: 'get',
+    url: `http://localhost:3000/products/${id}`
+  };
+  const styles = {
+    method: 'get',
+    url: `http://localhost:3000/products/${id}/styles`
+  };
+  const related = {
+    method: 'get',
+    url: `http://localhost:3000/products/${id}/related`
+  };
+
+  const res = http.batch([product, styles, related]);
+
+  check(res[0], {
+    'main product status was 200': (r) => r.status === 200,
+  });
+  // sleep(1);
 };
