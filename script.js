7353f4be96a535428c9bd54cc8222654d7dccee5
Joy <parkerjn90@gmail.com>
2022-06-14 12:17:17 -0400

    update script file to test a few more times to get average req duration

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-   { duration: '10s', target: 500 }, // below normal load
+   { duration: '10s', target: 500 },
    { duration: '10s', target: 100 },
-   { duration: '10s', target: 1000 }, // spike to 1400 users
-   { duration: '1m', target: 1000 }, // stay at 1400 for 3 minutes
-   { duration: '10s', target: 100 }, // scale down. Recovery stage.
+   { duration: '10s', target: 1000 },
+   { duration: '1m', target: 1000 },
+   { duration: '10s', target: 100 },
    { duration: '10s', target: 0 },
   ],
  };
 
 export default function () {
   var id = 1000011;
-  for (var i = 1000011; i > (1000011 * .9); i --) {
+  for (var i = 1000011; i > (1000011 * .9); i--) {
     id = i;
-  }
+  };
   const main = {
     method: 'get',
     url: `http://localhost:3000/products/${id}`
